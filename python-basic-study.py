cb70d3af7b172d100fe644d025ab26a582d362f3
KJ.2016 <charyj2000@yahoo.com>
2017-03-20 00:04:47 +0800

    python study case 1 for built-in objects.

---
+++ python-basic-study.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python
+
+#exec vim "$@"
+
+"""
+This module is used to demo the built-in objects in python.
+"""
+
+print("\'digit\', int, float...\t" + "1234, 3.1415, 3+4j, Decimal, Fraction" + "\r\n")
+print("\'string\', str class\t" + "\'spam\', \"guide\'s\", b\'a\xol\'")
